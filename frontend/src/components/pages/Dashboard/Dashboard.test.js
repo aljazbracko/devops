323464a9536c997097b8270ce9f455535cf3ee60
aljazbracko <aljaz.bracko@student.um.si>
2024-11-26 13:13:43 +0100

    test

--- frontend/src/components/pages/Dashboard/Dashboard.test.js
+++ frontend/src/components/pages/Dashboard/Dashboard.test.js
@@ -4,7 +4,7 @@ import Dashboard from './Dashboard';
 
 describe('Dashboard Component', () => {
   it('should render the employees table with correct headers', () => {
-    // Preveri, ali so pravilno prikazane glave tabele za pregled zaposlenih
+    // Preveri, ali so pravilno prikazane glave tabele za pregled zaposlenih  -- test za workflow
     render(<Dashboard />);
     expect(screen.getByText(/Ime/i)).toBeInTheDocument();
     expect(screen.getByText(/Skupno število ur/i)).toBeInTheDocument();
